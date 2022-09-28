e07e20b8c4811732471cbe826064ce91eca48e03
Ksenia0902 <computer.ksenia.kravchenko@gmail.com>
2022-09-29 00:54:56 +0300

    done second hw

---
+++ lesson2/js/script.js
@@ -0,0 +1,26 @@
+// first exercise
+
+let userAge = 16;
+console.log(userAge);
+
+let userFirstName = 'Ksenia';
+console.log(userFirstName);
+
+let userSecondName = 'Kravchenko';
+console.log(userSecondName);
+
+// second exercise
+
+let newUser;
+let shoppingList;
+let onlineUser;
+let workerSalary;
+let cursorCoordinates;
+
+// third exercise
+
+let text = 'Hello World!';
+console.log(text);
+
+const result = userFirstName + " " + userSecondName;
+console.log(result);
