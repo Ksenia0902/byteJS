52609cf3af84533a73142436d020a82df791dfc6
Ksenia0902 <computer.ksenia.kravchenko@gmail.com>
2023-01-09 18:10:27 +0200

    improve 12 hw

--- lesson12/script.js
+++ lesson12/script.js
@@ -1,22 +1,38 @@
 const createTooltip = (elem, text) => {
-
     const tooltip = document.createElement('div');
     tooltip.classList.add('tooltip');
 
     tooltip.innerText = text;
-
+    
     document.body.append(tooltip);
+    console.log(tooltip);
 
     const anchorElemCoords = elem.getBoundingClientRect();
-    
-    const tooltipTop = anchorElemCoords.top - 5 - tooltip.clientHeight;
+    console.log(anchorElemCoords);
 
+    const tooltipTop = anchorElemCoords.top - 5 - tooltip.clientHeight;
+    console.log(tooltipTop);
     const tooltipLeft = anchorElemCoords.left + anchorElemCoords.width / 2 - tooltip.clientWidth / 2;
+    console.log(tooltipLeft);
 
     tooltip.style.top = `${tooltipTop}px`;
-    tooltip.style.left = `${tooltipLeft}px`
-}
+    tooltip.style.left = `${tooltipLeft}px`;
+
+    tooltip.setAttribute('hidden', '');
+
+};
 
 const button = document.getElementById('example');
-
 createTooltip(button, 'example text bla bla bla');
+const tooltip = document.querySelector('.tooltip');
+
+const handleTooltipMouseover = () => {
+    tooltip.removeAttribute('hidden');
+};
+
+const handleTooltipMouseout = () => {
+    tooltip.setAttribute('hidden', '');
+}
+
+button.addEventListener('mouseover', handleTooltipMouseover);
+button.addEventListener('mouseout', handleTooltipMouseout);
